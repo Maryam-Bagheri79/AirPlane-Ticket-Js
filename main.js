b4f27c97cebb3e167249f48e2f465c12ca16f6b4
Maryam-Bagheri79 <m.bagheri7939@gmail.com>
2021-11-24 20:25:45 +0330

    end

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 //! Buy Tickets Online : 83
+
+let city = {
+    qom: ['قم', 'کهک', 'ونارچ', 'جعفریه'],
+    teh: ['ملارد', 'اسلام اباد', 'پاکدشت'],
+    esf: ['سلفچگان', 'دلیجان', 'گلپایگان'],
+}
+let startProvine = prompt('مبدا خود را وارد کنید')
+
+let provineArray = city[startProvine]
+
+
+provineArray.forEach(function (city) {
+    console.log(city)
+});
